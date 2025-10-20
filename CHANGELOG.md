17a1b09b5449d093d3ca76925a528e1f557cc08b
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-10-20 04:31:22 -0400

    See #243.  Added a function to retrieve the version number from the pyproject.toml.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is base on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 
+## [0.12.05.000] - 2025-10-20
+### Changed
+- Issue [#243](https://github.com/j3-signalroom/kafka_cluster-topics-partition_count_recommender-tool/issues/243)
+
 ## [0.12.04.000] - 2025-10-14
 ### Changed
 - Issue [#281](https://github.com/j3-signalroom/kafka_cluster-topics-partition_count_recommender-tool/issues/281)
--- src/utilities.py
+++ src/utilities.py
@@ -58,3 +58,21 @@ def setup_logging(log_file: str = DEFAULT_TOOL_LOG_FILE) -> logging.Logger:
         )
 
     return logging.getLogger()
+
+
+def get_app_versionsetup() -> str:
+    """Retrieve the application version string from pyproject.toml.
+
+    Return(s):
+        str: The application version string.
+    """
+    pyproject_path = Path("pyproject.toml")
+
+    if pyproject_path.exists():
+        with open(pyproject_path, "rb") as f:
+            config = tomllib.load(f)
+
+        # Extract version
+        return config.get("tool", {}).get("poetry", {}).get("version", "0.0.0")
+
+    return "0.0.0"
